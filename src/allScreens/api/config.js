e5e17b2da4bfc4205bef4a9c55b80fd605febdf1
ngodinhan058 <dinhanngo058@gmail.com>
2025-04-25 16:21:55 +0700

    complete UI

--- src/allScreens/api/config.js
+++ src/allScreens/api/config.js
@@ -1,3 +1,3 @@
 export const BE_URL = `http://192.168.1.195:5000`;
-export const WebView_URL = `http://192.168.1.195:3000`;
+export const WebView_URL = `https://ngodinhan058.github.io/webview-audio/`;
 export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwIjoxNzU2MzY2MjM5fQ.jgpb6Sjohqp_JlBeQ_ZBVy5lUQK7LBciRdzxprBEFxs';
