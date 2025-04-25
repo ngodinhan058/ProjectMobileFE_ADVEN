3d4eb1ae454ff29e570935281f72972fea4e60e5
ngodinhan058 <dinhanngo058@gmail.com>
2025-04-25 23:00:36 +0700

    update

--- src/allScreens/api/config.js
+++ src/allScreens/api/config.js
@@ -1,3 +1,3 @@
-export const BE_URL = `http://192.168.1.195:5000`;
+export const BE_URL = `https://1ce7-2405-4802-8151-df90-4844-f822-163f-8863.ngrok-free.app`;
 export const WebView_URL = `https://ngodinhan058.github.io/webview-audio/`;
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwIjoxNzU2MzY2MjM5fQ.jgpb6Sjohqp_JlBeQ_ZBVy5lUQK7LBciRdzxprBEFxs';
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwIjoxNzU2Mzc0NjczfQ.i-9E66nb-yTUMBGZ_T1lG_qWbwZvC2jpZ1vlZ6XjMi8';
